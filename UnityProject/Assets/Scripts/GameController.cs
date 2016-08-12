7caff630af0d7b41608d58eb37b29c9628ec1641
Joel Kuntz <joel.kuntz.92@gmail.com>
2016-08-12 20:29:31 -0300

    initial project commit

---
+++ UnityProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    public Text scoreText;
+    public GameObject pelletPrefab;
+    public Vector3 minPellet;
+    public Vector3 maxPellet;
+    public float spawnChance = 0.5f;
+
+    int score;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+            score = 0;
+            UpdateUI();
+            CreatePellets();
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        UpdateUI();
+    }
+
+    public void AddScore()
+    {
+        score++;
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    void CreatePellets()
+    {
+        for (int y = (int)minPellet.y; y <= (int)maxPellet.y; y++)
+        {
+            for (int x = (int)minPellet.x; x < (int)maxPellet.x; x++)
+            {
+                for (int z = (int)minPellet.z; z < (int)maxPellet.z; z++)
+                {
+                    if (Random.Range(0f, 1f) >= spawnChance)
+                    {
+                        Instantiate(pelletPrefab, new Vector3(x, y, z), Quaternion.identity);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Direction
+{
+    None,
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+public class Player : MonoBehaviour
+{
+    Direction nextMove;
+    Rigidbody rb;
+
+    public float speed = 1.0f;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        nextMove = Direction.None;
+    }
+
+    void Start()
+    {
+        applyRotationVelocity();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Up")) { nextMove = Direction.Up; }
+        if (Input.GetButtonDown("Down")) { nextMove = Direction.Down; }
+        if (Input.GetButtonDown("Left")) { nextMove = Direction.Left; }
+        if (Input.GetButtonDown("Right")) { nextMove = Direction.Right; }
+    }
+
+    void FixedUpdate()
+    {
+        switch (nextMove)
+        {
+            case Direction.Up:
+                transform.Rotate(-90, 0, 0);
+                nextMove = Direction.None;
+                applyRotationVelocity();
+                break;
+            case Direction.Down:
+                transform.Rotate(90, 0, 0);
+                nextMove = Direction.None;
+                applyRotationVelocity();
+                break;
+            case Direction.Left:
+                transform.Rotate(0, -90, 0);
+                nextMove = Direction.None;
+                applyRotationVelocity();
+                break;
+            case Direction.Right:
+                nextMove = Direction.None;
+                transform.Rotate(0, 90, 0);
+                applyRotationVelocity();
+                break;
+            case Direction.None: break;
+        }
+    }
+
+    void applyRotationVelocity()
+    {
+        Vector3 locVel = transform.InverseTransformDirection(Vector3.zero);
+        locVel.z = speed * Time.deltaTime;
+        rb.velocity = transform.TransformDirection(locVel);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pellet")
+        {
+            GameController.instance.AddScore();
+            Destroy(other.gameObject);
+        }
+        else if (other.tag == "Enemy")
+        {
+            //EXPLOSION
+            //GAME OVER
+            //SUCH LOSS
+        }
+    }
+}
